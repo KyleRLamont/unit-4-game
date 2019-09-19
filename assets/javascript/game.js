396de812a5a1bb487e3bdba8927b78936c04f0fc
Kyle LaMont <kylerlamont@gmail.com>
2019-09-19 11:35:12 -0700

    added wintext function and on click event and value assignment for each gem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,72 @@
 $(document).ready(function() {
 
     var targetNumber = 0;
-    targetNumber = Math.floor(Math.random()*120)+19;
+    targetNumber = Math.floor(Math.random()*101+19);
     console.log(targetNumber);
-    $("#number-to-guess").prepend(targetNumber);
+    $("#number-to-guess").text(targetNumber);
+    
 
-    var counter = 0;
+    var wins= 0;
+    var losses= 0;
+    var totalval= 0;
 
-    var numberOptions = [10, 5, 3, 7];
+    var crys1 = Math.floor(Math.random()*11+1);
+    var crys2 = Math.floor(Math.random()*11+1);
+    var crys3 = Math.floor(Math.random()*11+1);
+    var crys4 = Math.floor(Math.random()*11+1);
 
-    for (var i = 0; i < numberOptions.length; i++) {
-         var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystalimg");
-        imageCrystal.attr("src", "assets/images/bluegem.jpg");
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        $("#crystalimg").append(imageCrystal);
+    $("#blue").on("click", function() {
+        totalval = totalval + crys1;
+        $("#scoretext").text(totalval);
+        winningtest();
+    });
+
+    $("#green").on("click", function() {
+        totalval = totalval + crys2;
+        $("#scoretext").text(totalval);
+        winningtest();
+    });
+
+    $("#yellow").on("click", function() {
+        totalval = totalval + crys3;
+        $("#scoretext").text(totalval);
+        winningtest();
+    });
+
+    $("#red").on("click", function() {
+        totalval = totalval + crys4;
+        $("#scoretext").text(totalval);
+        winningtest();
+    });
+
+    function winningtest(){
+        if(totalval == targetNumber){
+            alert("You've won!");
+            wins++;
+            $("#wins").text(wins);
+            reset();
+
+        } else if(totalval < targetNumber){
+            stop();
+        
+        } else if(totalval > targetNumber) {
+            alert("Sorry, try again.");
+            losses++;
+            $("#losses").text(losses);
+            reset();
+        }
     }
+    
+
+    function reset(){
+        targetNumber = Math.floor(Math.random()*101+19);
+        console.log(targetNumber);
+        $("#number-to-guess").text(targetNumber);
+        crys1 = Math.floor(Math.random()*11+1);
+        crys2 = Math.floor(Math.random()*11+1);
+        crys3 = Math.floor(Math.random()*11+1);
+        crys4 = Math.floor(Math.random()*11+1);
+        totalval= 0;
+    }
+    
 });
