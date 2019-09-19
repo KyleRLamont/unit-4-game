454168d48a5530c5679be7ce84680dcc5a0264f8
Kyle LaMont <kylerlamont@gmail.com>
2019-09-19 09:05:15 -0700

    target number now populates in appropriate field, blue crystal now populates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,19 @@
-var targetNumber = 0;
-targetNumber = Math.floor(Math.random()*120)+19;
-console.log(targetNumber)
-$("#number-to-guess").html(targetNumber);
+$(document).ready(function() {
 
-var counter = 0;
+    var targetNumber = 0;
+    targetNumber = Math.floor(Math.random()*120)+19;
+    console.log(targetNumber);
+    $("#number-to-guess").prepend(targetNumber);
 
-var numberOptions = [10, 5, 3, 7];
+    var counter = 0;
 
-for (var i = 0; i < numberOptions.length; i++) {
-  var imageCrystal = $("<img>");
-  imageCrystal.addClass("crystal-image");
-  imageCrystal.attr("src", "assets/images/bluegem.jpg");
-  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-  $("#crystals").append(imageCrystal);
-}
+    var numberOptions = [10, 5, 3, 7];
+
+    for (var i = 0; i < numberOptions.length; i++) {
+         var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystalimg");
+        imageCrystal.attr("src", "assets/images/bluegem.jpg");
+        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+        $("#crystalimg").append(imageCrystal);
+    }
+});
